ca995b39fe6c89f48117af4cf76ee0cabe54cefd
Priscilasilvaa <pfernanda21@yahoo.com.br>
2024-07-02 20:21:46 -0300

    4

--- assets/icons.js
+++ assets/icons.js
@@ -2,7 +2,6 @@ import { AntDesign, Feather } from "@expo/vector-icons";
 
 export const icons = {
     index: (props)=> <AntDesign name="home" size={24} {...props} />,
-    explore: (props)=> <Feather name="compass" size={24} {...props} />,
     create: (props)=> <AntDesign name="pluscircleo" size={24} {...props} />,
     profile: (props)=> <Feather name="map" size={24} {...props} />,
 }
